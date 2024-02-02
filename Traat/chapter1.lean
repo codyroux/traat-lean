1eca3e6eaefc1fe7dfc01ac3343c31bf1d6a0e89
Cody Roux <codyroux@amazon.com>
2024-02-01 22:59:32 -0500

    Finally finish proofs up to Newman

--- Traat/chapter1.lean
+++ Traat/chapter1.lean
@@ -38,9 +38,9 @@ inductive refl_trans_sym_clos (R  : A → A → Prop) : A → A → Prop :=
 
 set_option quotPrecheck false
 
-infix:60 " ⊆ " => (fun (R R' : A → A → Prop) => ∀ x y, R x y → R' x y)
-infix:60 " ≅ " => (fun (R R' : A → A → Prop) => ∀ x y, R x y ↔ R' x y)
-postfix:100 " ⁻¹ " => (fun (R : A → A → Prop) => fun x y => R y x)
+infix:60 " ⊆ " => (λ (R R' : A → A → Prop) ↦ ∀ x y, R x y → R' x y)
+infix:60 " ≅ " => (λ (R R' : A → A → Prop) ↦ ∀ x y, R x y ↔ R' x y)
+postfix:100 " ⁻¹ " => (λ (R : A → A → Prop) ↦ λ x y ↦ R y x)
 infixl:50 " ~> " => R
 
 infixl:50 " ~>+ "   => trans_clos R
@@ -167,6 +167,37 @@ lemma refl_trans_sym_is_trans_sym_refl :
           apply red
         . trivial
 
+lemma trans_is_refl_trans : ∀ x y : A, x ~>+ y → x ~>* y :=
+by
+  intros x y red
+  induction red
+  . apply refl_trans_clos.step; trivial
+    constructor
+  . case step red _ ih =>
+    apply refl_trans_clos.step <;> trivial
+
+lemma refl_trans_is_trans_or_eq : ∀ x y : A, x ~>* y → x = y ∨ x ~>+ y :=
+by
+  intros x y red
+  induction red
+  . left; trivial
+  . case step red _ ih =>
+    right; cases ih
+    . apply trans_clos.base
+      simp [*] at *; trivial
+    . apply trans_clos.step <;> trivial
+
+lemma refl_trans_step_is_trans : ∀ x y z : A, x ~> z → z ~>* y → x ~>+ y :=
+by
+  intros x y z red_x_z red_z_y
+  revert red_x_z x
+  induction red_z_y <;> intros x red_x_?
+  . constructor; trivial
+  . case step _ _ _ ih =>
+    apply trans_clos.step; trivial
+    apply ih; trivial
+
+
 lemma sym_inv : ∀ (R : A → A → Prop), R⁻¹ ⊆ sym_clos R :=
  by intros R x y red
     apply sym_clos.inv; trivial
@@ -190,7 +221,7 @@ by
     rw [←zero_case]
     apply incr_case
 
-
+-- An alternate way to define normality.
 lemma normal_red : ∀ x y, normal R x → x ~>* y → x = y :=
 by
   intros x y norm red
@@ -201,7 +232,7 @@ by
 
 #check forall_and
 
-
+-- I'm convinced this is somewhere in the standard library
 theorem exists_iff : ∀ T (P Q : T → Prop),
         (∀ t, Q t ↔ P t) →
         (∃ t, P t) →
@@ -233,12 +264,12 @@ lemma dependent_choice' : ∀ {A} (Q : A → A → Prop) (a : A),
 by
   intros A Q a forall_exists_Q
   have choice_aut : ∃ f : A → A, ∀ x, Q x (f x) :=
-  (by
+  by
     apply Classical.axiomOfChoice
-    exact forall_exists_Q)
+    exact forall_exists_Q
   match choice_aut with
    | Exists.intro f h =>
-       exists (λ n => f^[n] a)
+       exists (λ n ↦ f^[n] a)
        apply And.intro
        . simp [Nat.iterate]
        . intros n
@@ -258,8 +289,8 @@ lemma dependent_choice : ∀ {A}
   ∃ f : ℕ → A, ∀ n, Q (f n) (f (n+1)) :=
 by
   intros A Q inhab forall_exists_Q
-  have a : A := (by match inhab with | Inhabited.mk a => exact a)
-  have h := (dependent_choice' _ a forall_exists_Q)
+  have a : A := by match inhab with | Inhabited.mk a => exact a
+  have h := dependent_choice' _ a forall_exists_Q
   match h with
   | Exists.intro f h' =>
     exists f
@@ -277,7 +308,7 @@ by
   intros P Q a pa ex
   have h : ∃ (f : ℕ → {x : A // P x}), f 0 = ⟨a, pa⟩
            ∧ ∀ n, P (f n) ∧ Q (f n) (f (n+1)) :=
-  (by
+  by
     apply (@dependent_choice' {x : A // P x} (λ p q => P p ∧ Q p q) _)
     intros ap
     match ap with
@@ -286,9 +317,7 @@ by
       apply And.intro
       . exact p
       . match (ex a' p) with
-        | Exists.intro b ⟨pb, qb⟩ =>
-        exists (Subtype.mk b pb)
-   )
+        | Exists.intro b ⟨pb, qb⟩ => exists (Subtype.mk b pb)
   match h with
   | Exists.intro f ⟨zf, sf⟩ =>
     exists (λ n ↦ (f n).val)
@@ -296,8 +325,7 @@ by
     intros n; apply sf
 
 -- This is the "real" def of normalization, or at least the
--- one that's actually useful. It's equivalent, in fact, but we really
--- only need this direction.
+-- one that's actually useful.
 lemma normalizes_ind : ∀ P : A → Prop,
   (∀ x, (∀ y, x ~> y → P y) → P x) → ∀ x, normalizes R x → P x :=
 by
@@ -305,7 +333,7 @@ by
   apply Classical.byContradiction
   intro h; apply norm
   have h' : ∃ f : ℕ → A, f 0 = x ∧ ∀ n, ¬ P (f n) ∧ f n ~> f (n+1) :=
-  (by
+  by
     apply (dependent_choice''  (λ a ↦ ¬ P a))
     . exact h
     . intros a npa
@@ -315,7 +343,7 @@ by
       apply IH; intros y ry
       apply Classical.byContradiction; intros npy
       apply (p y npy ry)
-   )
+
   rw [diverge_from]
   match h' with
     | Exists.intro xs ⟨P0, P1⟩ =>
@@ -336,6 +364,23 @@ by
   . apply norm
 
 
+lemma ind_normalizing :
+   (∀ P : A → Prop, (∀ x, (∀ y, x ~> y → P y) → P x) → ∀ x, P x) → normalizing R :=
+by
+  intros ih
+  intros x
+  apply ih
+  intros x red_norm
+  intro div
+  cases div
+  case a.intro f h =>
+    apply red_norm
+    . simp [← h.1]
+      apply h.2 0
+    . exists (λ n ↦ f (n+1))
+      simp
+      intros n; apply h.2
+
 -- Should be easy!
 lemma red_ref_trans_clos : ∀ x y z, x ~> y → refl_trans_clos R y z → refl_trans_clos R x z :=
 by
@@ -353,12 +398,13 @@ by
   by_cases (∃ y, x ~> y)
   . cases' h with y h'
     cases' h _ h' with z h''
+    cases h''
     exists z
     constructor
     . apply refl_trans_clos.step
       . apply h'
-      . apply h''.1
-    . apply h''.2
+      . trivial
+    . trivial
   . exists x
     constructor
     . apply refl_trans_clos.refl
@@ -395,7 +441,7 @@ by
   apply refl_trans_sym_clos.base; trivial
 
 
-lemma wedge_inc_refl_sym_trans : wedge R ⊆ (fun x y ↦ x <~>* y) :=
+lemma wedge_inc_refl_sym_trans : wedge R ⊆ (. <~>* .) :=
   by
     intros x y wedge
     cases' wedge with w h
@@ -429,9 +475,7 @@ by
   cases' joins_y_z with w h
   cases h
   exists w; constructor
-  . apply refl_trans_clos.step
-    . trivial
-    . trivial
+  . apply refl_trans_clos.step <;> trivial
   . trivial
 
 lemma reds_joins_left : ∀ x y z, x ~>* y → y ~>*.*<~ z → x ~>*.*<~ z :=
@@ -542,11 +586,22 @@ by
 inductive X := | a | b | c | d
 
 -- a <- b <=> c -> d
-inductive RX : X → X → Prop:=
+
+-- This definition is a bit tedious because of dependent matching.
+/- inductive RX : X → X → Prop :=
   | r_b_a : RX b a
   | r_b_c : RX b c
   | r_c_b : RX c b
-  | r_c_d : RX d c
+  | r_c_d : RX c d
+ -/
+-- This one works great with simp and friends
+def RX : X → X → Prop :=
+  open X in
+  λ x y ↦
+    (x = b ∧ y = a)
+  ∨ (x = b ∧ y = c)
+  ∨ (x = c ∧ y = b)
+  ∨ (x = c ∧ y = d)
 
 lemma not_joins_a_d : ¬ joins RX X.a X.d :=
 by
@@ -557,18 +612,16 @@ by
     apply normal_red RX
     . intros h
       cases' h with w h
-      -- inversion h?
-      sorry
+      simp [RX] at h
     . apply h1
   have eq_d : X.d = w := by
     apply normal_red RX
     . intros h
       cases' h with w h
-      -- inversion h
-      sorry
+      simp [RX] at h
     . apply h2
-  congr -- this doesn't work!?
-  sorry
+  simp [← eq_a] at eq_d
+
 
 lemma weakly_confluent_does_not_imply_confluent : ∃ (A : Type) (R : A → A → Prop),
 weakly_confluent R ∧ ¬ confluent R :=
@@ -576,19 +629,151 @@ by
   exists X, RX
   constructor
   . intros x y z r_x_y r_x_z
-    sorry -- tedious; needs cases on every possible "peak"!
+    simp [RX] at r_x_y
+    simp [RX] at r_x_z
+    -- ugh this is tedious. Probably there's some nuclear tactic here
+    cases r_x_y <;> cases r_x_z <;> simp [*] at *
+    . exists X.a; repeat constructor
+    . exists X.a; try constructor
+      . constructor
+      . simp [*]; constructor; simp [RX]
+        . left; trivial
+        . constructor; simp [RX]; left; trivial; constructor
+    . exists X.a; simp [*]; constructor
+      . repeat (constructor; simp [RX]; left; trivial)
+        constructor
+      . constructor
+    . case inr h1 h2 =>
+      cases h1 <;> cases h2 <;> simp [*] at *
+      . exists X.c; repeat constructor
+      . case inr h1 h2 =>
+        cases h1 <;> cases h2 <;> simp [*] at *
+        . exists X.b; repeat constructor
+        . exists X.d; constructor
+          . constructor; simp [RX]; right; trivial
+            constructor; simp [RX]; right; trivial
+            constructor
+          . constructor
+        . exists X.d; constructor
+          . constructor
+          . constructor; simp [RX]; right; trivial
+            constructor; simp [RX]; right; trivial
+            constructor
+        . exists X.d; repeat constructor
   . intros h
     apply not_joins_a_d
     apply h
     exists X.b; constructor
     . constructor
-      apply RX.r_b_a
+      simp [RX]
+      left; trivial
       constructor
     . constructor
-      apply RX.r_b_c
+      simp [RX]
+      right; trivial
       constructor
+      . simp [RX]; right; trivial
+      . constructor
+
+
+inductive close_below (P : A → Prop) : A → Prop :=
+  | here : ∀ x, P x → close_below P x
+
+-- Sometimes it's easier to work with the transitive closure for WF induction.
+lemma normalizes_trans : normalizing R → normalizing (. ~>+ .) :=
+by
+  intros norm
+  apply ind_normalizing
+  intros P ih
+  let Q := λ x ↦ P x ∧ ∀ y, x ~>+ y → P y
+  have h : ∀ x, Q x :=
+  by
+    intros x
+    apply normalizing_ind R
+    . intros x ih'
+      constructor
+      . apply ih
+        intros y red_x_y
+        cases red_x_y
+        . apply (ih' _ _).1
+          trivial
+        . apply (ih' _ _).2 <;> trivial
+      -- FIXME: complete duplication!
+      . intros y red_x_y
+        cases red_x_y
+        . apply (ih' _ _).1
+          trivial
+        . apply (ih' _ _).2 <;> trivial
+    . trivial
+  intros x; apply (h x).1
+
+-- When proving confluence, it's actually tedious to always handle the reflexive case.
+def confluent' := ∀ x y z : A, x ~>+ y → x ~>+ z → y ~>*.*<~ z
+
+lemma confluent'_implies_confluent : confluent' R → confluent R :=
+by
+  intros conf'
+  apply semi_confluent_implies_confluent
+  intros x y z red_x_y red_x_z
+  have h := refl_trans_is_trans_or_eq _ _ _ red_x_z
+  cases h
+  . case a.inl h =>
+    simp [← h]
+    exists y; repeat constructor
+    trivial
+    constructor
+  . apply conf'
+    . constructor; trivial
+    . trivial
+
 
 -- but if R is normalizing...
-theorem newmanns_lemma : normalizing R → weakly_confluent R → confluent R :=
+theorem newmans_lemma : normalizing R → weakly_confluent R → confluent R :=
   -- Requires a clever induction on diagrams
-sorry
+by
+  intros norm_R weak
+  intros y z wedge
+  cases wedge
+  case intro x h =>
+  revert h z y
+  have norm_trans := normalizes_trans _ norm_R
+  have h :=
+    normalizes_ind (. ~>+ .)
+    (λ x ↦ ∀ y z, x ~>* y ∧ x ~>* z → y ~>*.*<~ z)
+  intros y z wedge
+  (apply h <;> try trivial) <;> try apply norm_trans
+  clear h
+  intros x ih y z wedge
+  -- FIXME: this is horrid
+  cases wedge.1
+  . exists z; constructor; apply wedge.2; constructor
+  . case a.step y' red_x_y' red_y'_y =>
+    cases wedge.2
+    . exists y; constructor; constructor
+      apply wedge.1
+    . case step z' red_x_z' red_z'_z =>
+      -- finally use weak confluence
+      have join1 := weak _ _ _ red_x_y' red_x_z'
+      cases' join1 with w h'
+      have join2 :=
+        ih y' (trans_clos.base _ _ red_x_y') y w
+          (And.intro red_y'_y h'.1)
+      have join3 := ih z' (trans_clos.base _ _ red_x_z') w z
+          (And.intro h'.2 red_z'_z)
+      cases' join2 with w1 h1
+      cases' join3 with w2 h2
+      have red_x_w : x ~>+ w :=
+        by
+          apply refl_trans_step_is_trans
+          . apply red_x_z'
+          . apply h'.2
+      have join4 := ih w red_x_w w1 w2
+        (And.intro h1.2 h2.1)
+      cases' join4 with omega h3
+      exists omega; constructor
+      . apply refl_trans_clos_transitive
+        . apply h1.1
+        . apply h3.1
+      . apply refl_trans_clos_transitive
+        . apply h2.2
+        . apply h3.2
